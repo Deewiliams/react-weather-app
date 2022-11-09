9db963443399e0a0d597ace1af6146005ad5aa46
Desire <desireirankundawilliams@gmail.com>
2022-11-09 15:02:23 +0200

    cleaned the code

--- src/Components/CurrentWeather/CurrentWeather.js
+++ src/Components/CurrentWeather/CurrentWeather.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -9,48 +9,52 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
   },
 }));
 
-const CurrentWeather = ({data}) => {
-    const classes = useStyles();
+const CurrentWeather = ({ data }) => {
+  const classes = useStyles();
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
           <Paper className={classes.paper}>
-          <Grid container spacing={3}>
-          <Grid item xs={6}>
-            <p style={{fontSize: 30}} >{data.city}</p>
-            <p style={{fontSize: 20}}>{data.weather[0].description}</p>
-            <p style={{fontSize: 70}}>{Math.round(data.main.temp)}°C</p>
-        </Grid>
-        <Grid item xs={6}>
-            <img  src={`icons/${data.weather[0].icon}.png`} alt="weather" />
-            <p style={{color: 'black',fontWeight: 'bold',fontSize: 30}}>Details</p>
-            <div style={{display: 'flex', justifyContent: 'space-around'}}>
-            <div>
-            <p>Feels like</p>
-            <p>Wind</p>
-            <p>humidity</p>
-            <p>pressure</p>
-            </div>
-            <div style={{color: 'black',fontWeight: 'bold'}}>
-            <p> {Math.round(data.main.feels_like)}°C</p>
-            <p>{data.wind.speed} m/s</p>
-            <p>{data.main.humidity}%</p>
-            <p>{data.main.pressure} hPa</p>
-            </div>
-            </div>
-        </Grid>
-          </Grid>
+            <Grid container spacing={3}>
+              <Grid item xs={6}>
+                <p style={{ fontSize: 30 }}>{data.city}</p>
+                <p style={{ fontSize: 20 }}>{data.weather[0].description}</p>
+                <p style={{ fontSize: 70 }}>{Math.round(data.main.temp)}°C</p>
+              </Grid>
+              <Grid item xs={6}>
+                <img src={`icons/${data.weather[0].icon}.png`} alt="weather" />
+                <p style={{ color: "black", fontWeight: "bold", fontSize: 30 }}>
+                  Details
+                </p>
+                <div
+                  style={{ display: "flex", justifyContent: "space-around" }}
+                >
+                  <div>
+                    <p>Feels like</p>
+                    <p>Wind</p>
+                    <p>humidity</p>
+                    <p>pressure</p>
+                  </div>
+                  <div style={{ color: "black", fontWeight: "bold" }}>
+                    <p> {Math.round(data.main.feels_like)}°C</p>
+                    <p>{data.wind.speed} m/s</p>
+                    <p>{data.main.humidity}%</p>
+                    <p>{data.main.pressure} hPa</p>
+                  </div>
+                </div>
+              </Grid>
+            </Grid>
           </Paper>
         </Grid>
       </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default CurrentWeather
+export default CurrentWeather;
