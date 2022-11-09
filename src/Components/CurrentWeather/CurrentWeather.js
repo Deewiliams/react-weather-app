d8db257e81ef0f06be762283ad2b4b86e1f375b5
Desire <desireirankundawilliams@gmail.com>
2022-11-09 15:01:38 +0200

    able to display weather details

--- src/Components/CurrentWeather/CurrentWeather.js
+++ src/Components/CurrentWeather/CurrentWeather.js
@@ -20,15 +20,30 @@ const CurrentWeather = ({data}) => {
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12
+          <Paper className={classes.paper}>
           <Grid container spacing={3}>
           <Grid item xs={6}>
-          <Paper className={classes.paper}>title</Paper>
+            <p style={{fontSize: 30}} >{data.city}</p>
+            <p style={{fontSize: 20}}>{data.weather[0].description}</p>
+            <p style={{fontSize: 70}}>{Math.round(data.main.temp)}°C</p>
         </Grid>
         <Grid item xs={6}>
-          <Paper className={classes.paper}>
             <img  src={`icons/${data.weather[0].icon}.png`} alt="weather" />
-          </Paper>
+            <p style={{color: 'black',fontWeight: 'bold',fontSize: 30}}>Details</p>
+            <div style={{display: 'flex', justifyContent: 'space-around'}}>
+            <div>
+            <p>Feels like</p>
+            <p>Wind</p>
+            <p>humidity</p>
+            <p>pressure</p>
+            </div>
+            <div style={{color: 'black',fontWeight: 'bold'}}>
+            <p> {Math.round(data.main.feels_like)}°C</p>
+            <p>{data.wind.speed} m/s</p>
+            <p>{data.main.humidity}%</p>
+            <p>{data.main.pressure} hPa</p>
+            </div>
+            </div>
         </Grid>
           </Grid>
           </Paper>
