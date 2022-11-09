ad9d3e9c6771a9f42329d1f202949171f86c5412
Desire <desireirankundawilliams@gmail.com>
2022-11-09 15:16:43 +0200

    able to display days

--- src/Components/Forecast/Forecast.js
+++ src/Components/Forecast/Forecast.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
-const Forecast = () => {
+const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+const Forecast = ({data}) => {
+    const dayInAWeek = new Date().getDay();
+    const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInAWeek));
+    console.log('days',forecastDays);
   return (
     <div>forecast</div>
   )
