0d57aaed7fba883036784be46a91b165e9f7e804
Desire <desireirankundawilliams@gmail.com>
2022-11-10 22:24:44 +0200

    cleaned the code

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
     method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'a9e410dbb9mshb438e1130de14b3p1925f8jsna6f44bf4f264',
+		'X-RapidAPI-Key': 'enter geo api key',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
   export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
   
   export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-  export const WEATHER_API_KEY = "d97899ab862d58a6f0df8d2f2971343d"; // enter your key from openweather API
+  export const WEATHER_API_KEY = "enter weather api key";
